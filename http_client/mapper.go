4e400aecdd948284b2da7f4625229a72ef1b30c7
lucasvmx <lucas.engen@hotmail.com>
2022-11-06 16:26:12 -0300

    feat: :sparkles: implementa salvamento de URL's em disco - implementa despacho de links para o downloader
    Signed-off-by: lucasvmx <lucas.engen@hotmail.com>

--- http_client/mapper.go
+++ http_client/mapper.go
@@ -1,9 +1,11 @@
 package http_client
 
 import (
+	"evm-downloader/filesystem"
 	"evm-downloader/model"
 	"fmt"
 	"log"
+	"os"
 	"strings"
 	"time"
 )
@@ -13,11 +15,39 @@ func addItemToMap(zoneCd, sectionNs, munCd, stateCd string) {
 	vscmrFileURL, localFilename := getVscmrFileURL(url, strings.ToLower(stateCd), munCd, zoneCd, sectionNs)
 	keyName := fmt.Sprintf("%v%v%v%v|%v", zoneCd, sectionNs, munCd, stateCd, localFilename)
 
+	urlChannel <- fmt.Sprintf("%v|%v", vscmrFileURL, localFilename)
+
 	globalMux.Lock()
 	urlMap[keyName] = vscmrFileURL
 	globalMux.Unlock()
 
 	<-threadsChan
+}
+
+func saveURLMapToDisk() {
+
+	var flags int = os.O_WRONLY
+
+	now := time.Now()
+	filename := fmt.Sprintf("url_maps_%v%v%v.dat", now.Day(), now.Month(), now.Year())
+
+	if filesystem.FileExists(filename) {
+		flags |= os.O_APPEND
+	} else {
+		flags |= os.O_CREATE
+	}
+
+	filePtr, err := os.OpenFile(filename, flags, os.ModePerm)
+	if err != nil {
+		log.Printf("[!] erro ao criar arquivo de mapas: %v", err)
+		return
+	}
+
+	defer filePtr.Close()
+
+	for _, url := range urlMap {
+		filePtr.WriteString(fmt.Sprintf("%v\n", url))
+	}
 }
 
 func CreateURLMap() {
@@ -38,8 +68,12 @@ func CreateURLMap() {
 			}
 
 			time.Sleep(time.Second * 5)
+			saveURLMapToDisk()
 		}
 	}
 
+	progressMux.Lock()
+	totalFilesToDownload = len(urlMap)
+	progressMux.Unlock()
 	log.Printf("[*] quantidade url's mapeadas: %v", len(urlMap))
 }
